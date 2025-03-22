0383e653d763be91c5c442ea552022e75ece6628
anurag <anuragdev03@gmail.com>
2025-03-22 20:02:17 +0530

    Added change password feature

--- src/features/settings/api/account.ts
+++ src/features/settings/api/account.ts
@@ -1,9 +1,21 @@
 import http from "../../../api/Client"
 
-
 export async function deleteAccount(password: string) {
     try {
         const res = await http.post("/expert/delete-account", {password}, {withCredentials: true});
+        return res?.data;
+    } catch(err:any) {
+        return err?.response?.data?.message
+    }
+}
+
+export async function updateExpertPassword(current_password: string, new_password: string) {
+    try {
+        const payload = {
+            current_password,
+            new_password
+        }
+        const res = await http.patch("/expert/change-password", payload, {withCredentials: true});
         return res?.data;
     } catch(err:any) {
         return err?.response?.data?.message
--- src/features/settings/index.tsx
+++ src/features/settings/index.tsx
@@ -1,17 +1,31 @@
-import { Box, Button, Group, Modal, Paper } from "@mantine/core"
+import { Box, Button, Divider, Group, Modal, Paper } from "@mantine/core"
 import "./styles/style.css"
 import PasswordInput from "../../components/PasswordInput"
 import { useState } from "react"
 import { notify } from "../../utilities/helpers";
 import { useDisclosure } from "@mantine/hooks";
-import { deleteAccount } from "./api/account";
+import { deleteAccount, updateExpertPassword } from "./api/account";
 import { useNavigate } from "react-router";
 import { getAccessToken } from "../../api/refreshToken";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface FormData {
+    current_password: string;
+    new_password: string;
+    confirm_password: string;
+}
 
 export default function Settings() {
     const [password, setPassword] = useState("");
     const [opened, { open, close }] = useDisclosure(false);
     const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors }, setError, reset } = useForm<FormData>({
+        defaultValues: {
+            current_password: "",
+            new_password: "",
+            confirm_password: ""
+        }
+    })
 
     function handleDelete() {
         if (!password) {
@@ -35,9 +49,30 @@ export default function Settings() {
             notify("ERROR", res);
             navigate("/login")
         }
-        if(res.message === "Account deleted successfully") {
+        if (res.message === "Account deleted successfully") {
             notify("SUCCESS", "Account deleted successfully");
             navigate("/")
+        }
+    }
+
+    const handlePasswordUpdate :SubmitHandler<FormData> = async (data) => {
+        if(data.new_password !== data.confirm_password) {
+            setError("confirm_password", {message: "New password and confirm password should be same"});
+            return
+        }
+
+        const res = await updateExpertPassword(data.current_password, data.new_password);
+        if (res === "Token has expired") {
+            const token = await getAccessToken();
+            if (token) handlePasswordUpdate(data);
+        }
+        if (res === "Please login again") {
+            notify("ERROR", res);
+            navigate("/login")
+        }
+        if(res.message === "Password changed successfully") {
+            notify("SUCCESS", res.message)
+            reset()
         }
     }
     return (
@@ -45,15 +80,54 @@ export default function Settings() {
             <Box>
                 <h3 className="settings-title">Settings</h3>
                 <Paper shadow="sm" radius={8} className="settings-paper">
-                    <p className="delete-title">Delete Account</p>
+                    <p className="sub-title">Delete Account</p>
                     <PasswordInput
                         label="Password"
                         placeholder="Enter password to confirm delete"
                         value={password}
                         onChange={handleChange}
                     />
-                    <Button onClick={handleDelete} color="red" my={16}>Delete Account</Button>
+                    <Button size="xs" onClick={handleDelete} color="red" my={16}>Delete Account</Button>
 
+                    <Divider mb={16} />
+                    <p className="sub-title">Change Password</p>
+                    <form onSubmit={handleSubmit(handlePasswordUpdate)}>
+                        <PasswordInput
+                            label="Current Password"
+                            required
+                            placeholder="Enter Current Password"
+                            mb={12}
+                            {...register("current_password", {
+                                required: "Current is required",
+                                minLength: { value: 12, message: "Password must be at least 12 characters" },
+                            })}
+                            error={errors.current_password ? errors.current_password?.message : ""}
+                        />
+                        <PasswordInput
+                            label="New Password"
+                            required
+                            placeholder="Enter New Password"
+                            mb={12}
+                            {...register("new_password", {
+                                required: "New Password is required",
+                                minLength: { value: 12, message: "New Password must be at least 12 characters" },
+                            })}
+                            error={errors.new_password ? errors.new_password?.message : ""}
+                        />
+                        <PasswordInput
+                            label="Confirm New Password"
+                            required
+                            placeholder="Confirm New Password"
+                            mb={12}
+                            {...register("confirm_password", {
+                                required: "Confirm New Password is required",
+                                minLength: { value: 12, message: "New Password must be at least 12 characters" },
+                            })}
+                            error={errors.confirm_password ? errors.confirm_password?.message : ""}
+                        />
+
+                        <Button type="submit" color="#000" size="xs">Update Password</Button>
+                    </form>
                     <Modal onClose={close} opened={opened} title="Confirm Delete" centered>
                         <p className="delete-info-text">Are you sure you want to delete your account? This action is permanent and cannot be undone. All your data, including saved preferences and history, will be lost forever.
 
